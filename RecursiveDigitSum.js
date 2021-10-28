c395a4fb3b87758c14278d9672edd25fbdd507ee
Divine Higgins <dhiggins94@gmail.com>
2021-10-27 20:00:52 -0400

    got bodied might need to check re read problem

---
+++ RecursiveDigitSum.js
@@ -0,0 +1,15 @@
+function superDigit(n, k) {
+  // Write your code here
+console.log(n) //148 string n: a string representation of an integer
+console.log(k) //int k: the times  to concatenate N to make P
+
+// let arr = Array.from(n)  maybe not use array from, turn it into a number of arrays
+let spread = [...arr]
+console.log(spread)
+
+const reducer = (a, b) => a + b;
+
+console.log(spread.reduce(reducer))
+
+}
+// unfinished
